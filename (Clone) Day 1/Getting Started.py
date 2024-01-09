3de0699b7c284f5c9e8a78596276a2751415728e
manojkumaracharya <manojkumaracharya>
2024-01-09 09:30:01 +0000

    new commit

---
+++ (Clone) Day 1/Getting Started.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+print("run python")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC print("Run SCALA")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- select "RUN SQL"
+
+# COMMAND ----------
+
+# MAGIC %md # TITLE
